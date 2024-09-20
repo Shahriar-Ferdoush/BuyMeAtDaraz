932831b5aab6daeb6eb1ef485263c5f1018160b6
Shahriar-Ferdoush <1805101@ugrad.cse.buet.ac.bd>
2024-09-20 18:19:21 +0600

    Daraz sync scrapper

--- buymeatdaraz/scrapper/daraz.py
+++ buymeatdaraz/scrapper/daraz.py
@@ -2,16 +2,17 @@ import asyncio
 
 import pandas as pd
 from playwright.async_api import async_playwright
+from playwright.sync_api import sync_playwright
 
 
-async def scrape_daraz():
+async def async_daraz_scraper(url: str):
     async with async_playwright() as pw:
         # Launch new browser
-        browser = await pw.chromium.launch(headless=False)
+        browser = await pw.chromium.launch(headless=True)
         page = await browser.new_page()
         # Go to Daraz URL
         await page.goto(
-            "https://www.daraz.com.bd/catalog/?spm=a2a0e.searchlist.search.d_go.1fee364apvjEuu&q=men%20watch",
+            url=url,
             timeout=100000,
         )
 
@@ -63,7 +64,72 @@ async def scrape_daraz():
         return results
 
 
+def sync_daraz_scraper(url: str):
+    with sync_playwright() as pw:
+        # Launch new browser
+        browser = pw.chromium.launch(headless=True)
+        page = browser.new_page()
+        # Go to Daraz URL
+        page.goto(
+            url=url,
+            timeout=100000,
+        )
+
+        # Extract information
+        results = []
+        listings = page.query_selector_all('div[data-qa-locator="product-item"]')
+        for listing in listings:
+            result = {}
+
+            # Title
+            title_element = listing.query_selector("div.RfADt > a")
+            result["title"] = (
+                title_element.get_attribute("title") if title_element else "N/A"
+            )
+
+            # Price
+            price_element = listing.query_selector("div.aBrP0 > span.ooOxS")
+            result["price"] = price_element.inner_text() if price_element else "N/A"
+
+            # Offer Percentage
+            offer_element = listing.query_selector("div.WNoq3 > span.IcOsH")
+            result["offer_percentage"] = (
+                offer_element.inner_text() if offer_element else "N/A"
+            )
+
+            # Number of sold items
+            sold_element = listing.query_selector("div._6uN7R > span._1cEkb > span")
+            result["sold"] = sold_element.inner_text() if sold_element else "N/A"
+
+            # Product link
+            link_element = listing.query_selector("div.RfADt > a")
+            if link_element:
+                relative_link = link_element.get_attribute("href")
+                result["link"] = f"https:{relative_link}"
+            else:
+                result["link"] = "N/A"
+
+            # Only add the result if it contains valid data
+            if result["title"] != "N/A" or result["price"] != "N/A":
+                results.append(result)
+
+        # Close browser
+        browser.close()
+
+        return results
+
+
 # Run the scraper and save results to a CSV file
-results = asyncio.run(scrape_daraz())
+# results = asyncio.run(
+#     async_daraz_scraper(
+#         "https://www.daraz.com.bd/catalog/?spm=a2a0e.searchlist.search.d_go.1fee364apvjEuu&q=men%20watch"
+#     )
+# )
+
+results = sync_daraz_scraper(
+    "https://www.daraz.com.bd/catalog/?spm=a2a0e.searchlist.search.d_go.1fee364apvjEuu&q=men%20watch"
+)
+
+
 df = pd.DataFrame(results)
 df.to_csv("output/daraz_products_listings.csv", index=False)
